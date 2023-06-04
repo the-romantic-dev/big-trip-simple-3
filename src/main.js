01e18e237eeab70caade9f998a1d39d674f6d3c7
kirikF <ahxplay@gmail.com>
2023-06-04 14:36:30 +0300

    Тяжко

--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import Presenter from './presenter/presenter';
 const tripEventsElement = document.querySelector('.trip-events');
 const tripControlsFiltersElement = document.querySelector('.trip-controls__filters');
 
-const presenter = new Presenter();
-presenter.init(tripControlsFiltersElement, tripEventsElement);
+const presenter = new Presenter(tripControlsFiltersElement, tripEventsElement);
+presenter.init();
---
+++ src/presenter/event_presenter.js
@@ -0,0 +1,61 @@
+import EventView from '../view/event_view.js';
+import EditingEventView from '../view/editing_event_view.js';
+
+export default class EventPresenter {
+
+  #event;
+  #currentView;
+  constructor(event, resetListCallback) {
+    this.#event = event;
+    this.editingEventView = new EditingEventView(this.event);
+    this.eventView = new EventView(this.event);
+    this.resetListCallback = resetListCallback;
+    this.#currentView = this.eventView;
+  }
+
+  get event() {
+    return this.#event;
+  }
+
+  get currentView() {
+    return this.#currentView;
+  }
+
+  replaceEventWithEditingForm() {
+    this.eventView.element.replaceWith(this.editingEventView.element);
+    this.#currentView = this.editingEventView;
+  }
+
+  replaceEditingFormWithEvent() {
+    this.editingEventView.element.replaceWith(this.eventView.element);
+    this.#currentView = this.eventView;
+  }
+
+  #addListenersForListElement() {
+    const editingEventViewListener = () => {
+
+      this.replaceEditingFormWithEvent();
+      document.removeEventListener('keydown', escListener);
+    };
+
+    function escListener (event) {
+      if (event.key === 'Escape') {
+        editingEventViewListener();
+      }
+    }
+
+    this.eventView.addButtonClickListener(() => {
+      this.resetListCallback();
+      this.replaceEventWithEditingForm();
+      document.addEventListener('keydown', escListener);
+    });
+
+    this.editingEventView.addButtonClickListener(editingEventViewListener);
+    this.editingEventView.addSubmitListener(editingEventViewListener);
+  }
+
+  createNewEventViewElement() {
+    this.#addListenersForListElement();
+    return this.eventView.element;
+  }
+}
